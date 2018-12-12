a98c56c109a49fa932f4ebdfd90954e8921aeb76
gcupani <cupani@oats.inaf.it>
2018-12-12 15:49:31 +0100

    Adding modeling of fiber losses

--- espr_eff.py
+++ espr_eff.py
@@ -16,9 +16,10 @@ from astropy.time import Time
 from formats import ESOExt, ESOStd, EsprEff, EsprS1D
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-from plots import Plot21
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from scipy.signal import savgol_filter as sg
+from toolbox import moffat_ee
 
 def binspec(wave, flux, bins=np.arange(360.0, 800.0, 1.6)*u.nm,
             binsize=1.6*u.nm):
@@ -33,8 +34,9 @@ def binspec(wave, flux, bins=np.arange(360.0, 800.0, 1.6)*u.nm,
         #bin_spec.append(np.median(flux[binw])*len(flux[binw]))
     return np.array(bin_spec)
 
-def run(**kwargs):
-
+def extract(**kwargs):
+    """ Extract the efficiency from a list of S1D_ frames """
+    
     # Load parameters
     frames = np.array(ascii.read(kwargs['framelist'],
                                  format='no_header')['col1'])
@@ -63,7 +65,8 @@ def run(**kwargs):
 
         # Correct observed spectrum for extinction
         la_silla_spec = np.interp(spec.wave, ext.wave, ext.la_silla)
-        spec.flux = spec.flux * 10 ** (0.4*la_silla_spec*(spec.airm-1))
+        #spec.flux = spec.flux * 10 ** (0.4*la_silla_spec*(spec.airm-1))
+        spec.flux = spec.flux * 10 ** (0.4*la_silla_spec*(spec.airm))
 
         # Load pipeline efficiency
         try:
@@ -151,7 +154,7 @@ def run(**kwargs):
 
         # Global plot
         if avecomp:
-            label = avetarg+', '+spec.date[:10]+', '+avetime+', IQ: '+aveiq
+            label = avetarg+', '+avetime[:10]+', '+avetime+', IQ: '+aveiq
             eff_ave = np.average(eff_save, axis=0)
             eff_std = np.std(eff_save, axis=0)
             if 'eff_ref' not in locals():
@@ -164,7 +167,20 @@ def run(**kwargs):
             axg[1].fill_between(bins.value, (eff_ave-eff_std)/eff_ref,
                                 (eff_ave+eff_std)/eff_ref,
                                 facecolor='C'+str(avecolor), alpha=0.3)
-        axg[0].plot(bins, eff_smooth, c='C'+str(color), linestyle=':')
+
+            # Save averages
+            hdu0 = fits.PrimaryHDU()
+            col0 = fits.Column(name='wave', format='D', array=bins)
+            col1 = fits.Column(name='eff_ave', format='D', array=eff_ave)
+            col2 = fits.Column(name='eff_std', format='D', array=eff_std)
+            hdu1 = fits.BinTableHDU.from_columns([col0, col1])
+            hdul = fits.HDUList([hdu0, hdu1])
+            hdul.writeto(kwargs['framelist'][:-4]+'_'+avetime[:10]+'.fits',
+                         overwrite=True)
+            print "...saved average efficiencies as", \
+                kwargs['framelist'][:-4]+'_'+avetime[:10]+'_eff.fits'+"."
+            
+        #axg[0].plot(bins, eff_smooth, c='C'+str(color), linestyle=':')
         
     if len(np.unique(binx_arr)) == 1:
         figg.suptitle(str(spec.binx)+'x'+str(spec.biny))
@@ -177,15 +193,39 @@ def run(**kwargs):
         plt.show()
     if save:
         figg.savefig(kwargs['framelist'][:-4]+'.pdf', format='pdf')
+
     plt.close()
+
+def model(**kwargs):
+    """ Model the efficiency from a set of previously produced 
+    *_list_YYYY-MM-DD.fits frames """
+
+    fig = plt.figure(figsize=(18,8))
+    gs = gridspec.GridSpec(2, 2)
+    ax = []
+    ax.append(fig.add_subplot(gs[0:2,0], projection='3d'))
+    ax.append(fig.add_subplot(gs[0,1]))
+    #ax0 = fig.add_subplot(211, projection='3d')
+    #ax1 = fig.add_subplot(212)
+    for fwhm in [0.4, 0.7]:
+        m = moffat_ee(fwhm)
+        ax[0].plot_surface(m.x, m.y, m.z)
+        ax[0].plot_surface(m.x, m.y, m.zin)
+        ax[1].plot(m.rad, m.ee)
+    plt.show()
         
 def main():
     """ Read the CL parameters and run """
 
     p = argparse.ArgumentParser()
+    p.add_argument('-m', '--mode', type=str, default='extract',
+                   help="Mode: 'extract' or 'model'.")
     p.add_argument('-l', '--framelist', type=str, default='frame_list.dat',
                    help="List of frames; must be an ascii file with a column "
-                   "of entries. ")
+                   "of entries. If mode is 'extract', the frames must be S1D "
+                   "frames from the DRS; if mode is 'model', they must be "
+                   "*_list_YYYY-MM-DD.fits frames produced in a previous "
+                   "execution of espr_eff.py.")
     #p.add_argument('-r', '--red', type=str,
     #                    help="Reduced star spectrum.")
     p.add_argument('-c', '--cal', type=str,
@@ -199,7 +239,11 @@ def main():
                    help="Save plot and table with measured efficiencies.")
 
     args = vars(p.parse_args())
-    run(**args)
+
+    if args['mode'] == 'extract':
+        extract(**args)
+    elif args['mode'] == 'model':
+        model(**args)
     
 if __name__ == '__main__':
     main()
--- toolbox.py
+++ toolbox.py
@@ -1,8 +1,10 @@
 from astropy import units
 from astropy.constants import c
 from astropy.coordinates import SkyCoord, EarthLocation
+from astropy.modeling.functional_models import Moffat2D
 #from astropy.time import Time, TimeDelta
 import barycen
+import numpy as np
 
 def air_to_vacuum(spec):
     """ Air to vacuum correction 
@@ -29,3 +31,38 @@ def earth_to_bary(spec, site_name='paranal'):
     barycor_vel = barycen.compute_barycentric_correction(spec.midtime, target,
                                                          location=site)
     spec.wave = spec.wave * (1 + barycor_vel/c).value
+
+def moffat_ee(fwhm=1.0, alpha=3.0, max_aperture=3.0):
+    """ Compute the encircled energy for a Moffat profile, given the FWHM """
+
+    # Find the value of Moffat gamma that gives the required FWHM 
+    gamma_arr = np.arange(fwhm*0.5, fwhm*2, fwhm*1e-2)
+    m_arr = Moffat2D(1.0, 0.0, 0.0, gamma_arr, alpha)
+    gamma = np.interp(fwhm, m_arr.fwhm, gamma_arr)
+
+    # Create a Moffat profile with the required FWHM
+    m = Moffat2D(1.0, 0.0, 0.0, gamma, alpha)
+    grid = np.arange(0, max_aperture, 1e-2)
+    grid_ext = np.arange(0, max_aperture*10, 1e-2)
+    x, y = np.meshgrid(grid, grid)
+    x_ext, y_ext = np.meshgrid(grid_ext, grid_ext)
+    m.x = x
+    m.y = y
+    m.z = m(x, y)
+    zsum = np.sum(m(x_ext, y_ext))
+
+    # Compute the encircled energy
+    m.ee = []
+    m.rad = np.arange(0, max_aperture, 1e-2)
+    for r in m.rad:
+        where = x**2 + y**2 < r**2
+        m.zin = np.zeros((len(x), len(y)))
+        m.zin[where] = m.z[where]
+    
+        # Compute the encircled energy
+        m.ee.append(np.sum(m.zin)/zsum)
+    return m
+
+    
+    
+    
